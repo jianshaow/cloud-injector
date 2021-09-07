468348eea67eaf18fbce85edfd04d6f8fa37c0f9
Jianshao <787903+jianshaow@users.noreply.github.com>
2021-09-07 15:31:10 +0800

    improve app logic

--- demo/original/www/app.py
+++ demo/original/www/app.py
@@ -1,13 +1,30 @@
 from flask import Flask, request, render_template
+import json
+import sys
 
 app = Flask(__name__)
 
 
-@app.route('/index', methods=['GET'])
+@app.route('/', methods=['GET'])
 def callback():
-    user_id = request.args['userId']
-    return render_template('index.tmpl', user_id=user_id, name="Bob", email="bob@abc.com")
+    user_id = request.args.get("userId")
+    if user_id:
+        user = user_info.get(user_id)
+        if user:
+            return render_template('index.tmpl', user=user)
+        else:
+            return render_template('index.tmpl', error="no user with id: "+user_id)
+    else:
+        return render_template('index.tmpl')
+
+
+def load_user_info(data_file):
+    with open(data_file, 'r') as user_data:
+        global user_info
+        user_info = json.load(user_data)
 
 
 if __name__ == '__main__':
+    data_file = (len(sys.argv) == 2 and sys.argv[1] or "/data/users.json")
+    load_user_info(data_file)
     app.run(debug=True, host="0.0.0.0")
